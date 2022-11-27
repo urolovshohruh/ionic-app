e59bcd02780c98e61cde100b570eafbd0bc3254e
Ismoil <maxammatovismol154@gmail.com>
2022-11-27 13:31:47 +0500

    remove folder

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import TabsPage from '../views/tabs/TabsPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -22,15 +22,15 @@ component: ()=> import("@/components/Advertising-department/Advertising-depart.v
 
       {
         path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
+        component: () => import('@/views/tabs/Tab1Page.vue')
       },
       {
         path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
+        component: () => import('@/views/tabs/Tab2Page.vue')
       },
       {
         path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        component: () => import('@/views/tabs/Tab3Page.vue')
       }
     ]
   }
